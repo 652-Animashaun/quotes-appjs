ff3c3c2d7428adb512f3e398a8731562198c2263
Marcus <oluwaseunmarcus77@gmail.com>
2024-05-30 23:00:35 +0100

    pagefile

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,30 @@
-import Search from './ui/search';
-import QuoteCardList from './ui/QuoteCardList';
-import {getQuotes} from './actions/fetchQuotes';
+import Search from './ui/search'
+import QuoteCardList, InfiniteScroll from './ui/QuoteCardList'
+import { getQuotes } from './actions/fetchQuotes'
 // import QuotesPage from './frontpage/page';
-import {QuoteListSkeleton} from '@/app/ui/skeletons';
-import { Suspense } from 'react';
+import { QuoteListSkeleton } from '@/app/ui/skeletons'
+import { Suspense } from 'react'
 
-
- 
 export default async function Page({
   searchParams,
 }: {
   searchParams?: {
-    q?: string;
-    page?: string;
-  };
+    q?: string
+    page?: string
+  }
 }) {
-
-
-  const query = searchParams?.q || '';
+  const query = searchParams?.q || ''
   // console.log('QueryTerms?', query)
-  const currentPage = Number(searchParams?.page) || 1;
+  const currentPage = Number(searchParams?.page) || 1
   const data = await getQuotes(query, currentPage)
-  
+
   return (
-          <main>
-            <Search />
-            {/*<QuoteListSkeleton/>*/}
-            <Suspense key={data.quotes} fallback={<QuoteListSkeleton />}>
-              <QuoteCardList quotes={data.quotes}/>
-            </Suspense>
-          </main>
-          )
-        
+    <main>
+      <Search />
+      {/*<QuoteListSkeleton/>*/}
+      <Suspense key={data.quotes} fallback={<QuoteListSkeleton />}>
+        <QuoteCardList quotes={data.quotes} />
+      </Suspense>
+    </main>
+  )
 }
